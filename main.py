55e75658ab9b497af6af39a5c410e2185ff9f7bf
jungeunji <ejjung312@gmail.com>
2025-04-16 20:59:16 +0900

    FastAPI upload 세팅

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Form, File
+from fastapi.responses import JSONResponse
+import numpy as np
+import cv2
 
 app = FastAPI()
 
@@ -7,6 +11,18 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/api/ocr/plate")
+async def read_item(image: UploadFile = File(...)):
+    try:
+        # 이미지 로드
+        file_data = await image.read()
+        nparr = np.frombuffer(file_data, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        if img is None:
+            return JSONResponse(status_code=400, content={"message": "이미지 디코딩 실패"})
+
+        return {"message": "성공"}
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
