4dc326f515edc2cd6953364a3f381950b67fcbe3
jungeunji <ejjung312@gmail.com>
2025-04-17 08:09:11 +0900

    API OCR 검출 성공

--- main.py
+++ main.py
@@ -24,12 +24,16 @@ async def read_item(image: UploadFile = File(...)):
 
         result = ocr.ocr(img, det=False, cls=False)
 
-        for idx in range(len(result)):
-            res = result[idx]
-            for line in res:
-                print(line[0])
+        license_plate_text = ''
+        if result:  # 결과가 비어있지 않다면
+            lines = result[0]  # 첫 번째 OCR 결과만 사용
+            for line in lines:
+                license_plate_text = line[0]
+                print(license_plate_text)
+        else:
+            print("OCR 결과 없음")
 
-        return {"message": "성공"}
+        return {"message": "OK", "license_plate_text": license_plate_text}
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": str(e)})
