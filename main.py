7a59821086c3f4b989561138971e7f9765f53d11
jungeunji <ejjung312@gmail.com>
2025-04-16 21:59:45 +0900

    PaddleOCR 추가

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from typing import Union
-from fastapi import FastAPI
 from fastapi import FastAPI, UploadFile, Form, File
 from fastapi.responses import JSONResponse
+from paddleocr import PaddleOCR
 import numpy as np
 import cv2
 
 app = FastAPI()
+ocr = PaddleOCR(lang='korean')
 
 @app.get("/")
 def read_root():
@@ -21,6 +21,13 @@ async def read_item(image: UploadFile = File(...)):
 
         if img is None:
             return JSONResponse(status_code=400, content={"message": "이미지 디코딩 실패"})
+
+        result = ocr.ocr(img, det=False, cls=False)
+
+        for idx in range(len(result)):
+            res = result[idx]
+            for line in res:
+                print(line[0])
 
         return {"message": "성공"}
 
