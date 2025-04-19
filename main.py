81efe4357aa7873c4cdbaaa480c1b2ad1d047b4c
jungeunji <ejjung312@gmail.com>
2025-04-19 11:01:13 +0900

    API OCR 검출 성공

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import database
 from database import engine, Base
 from models import *
 from crud import *
-# from paddleocr import PaddleOCR
+from paddleocr import PaddleOCR
 import numpy as np
 import cv2
 
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-# ocr = PaddleOCR(lang='korean')
+ocr = PaddleOCR(lang='korean')
 
 Base.metadata.create_all(bind=engine)
 
@@ -45,23 +45,24 @@ async def read_item(image: UploadFile = File(...), db: Session = Depends(get_db)
         if img is None:
             return JSONResponse(status_code=400, content={"message": "이미지 디코딩 실패"})
 
-        license_plate_text = '12가3457'
-        # result = ocr.ocr(img, det=False, cls=False)
-        #
-        # if result:  # 결과가 비어있지 않다면
-        #     lines = result[0]  # 첫 번째 OCR 결과만 사용
-        #     for line in lines:
-        #         license_plate_text = line[0]
-        #         print(license_plate_text)
-        # else:
-        #     print("OCR 결과 없음")
+        # license_plate_text = '12가3457'
+        license_plate_text = ''
+        is_exist = True
+        result = ocr.ocr(img, det=False, cls=False)
 
-        db_lp = get_license_plate(db, license_plate_text)
-        if db_lp is None:
-            create_license_plate(db, license_plate_text)
-            is_exist = False
+        if result:  # 결과가 비어있지 않다면
+            lines = result[0]  # 첫 번째 OCR 결과만 사용
+            for line in lines:
+                license_plate_text = line[0]
+                print(license_plate_text)
         else:
-            is_exist = True
+            print("OCR 결과 없음")
+
+        if license_plate_text != '':
+            db_lp = get_license_plate(db, license_plate_text)
+            if db_lp is None:
+                create_license_plate(db, license_plate_text)
+                is_exist = False
 
         return {"message": "OK", "is_exist": is_exist, "license_plate_text": license_plate_text}
 
